ff25c75ae66fb7f0d48d8f76fef137574f52673d
Jerome Dela Cruz <jeromedelacruz@Jeromes-MacBook-Pro.local>
2019-07-08 12:16:39 -0500

    Finished the cities exercise\! It was pretty difficult to figure out how to access the array and nested objects but the key thing was you were looking at the array wrong\! Got it right though.

--- exercises/220-cities.js
+++ exercises/220-cities.js
@@ -7,9 +7,35 @@
 // ];
 // and returns a new array containing only those cities whose temperature is
 // cooler than 70 degrees.
-
-
-
+function coolCities(){
+  var cities = [
+   { name: 'Los Angeles', temperature: 60.0},
+   { name: 'Atlanta', temperature: 52.0 },
+   { name: 'Detroit', temperature: 48.0 },
+   { name: 'New York', temperature: 80.0 }
+ ]
+  let results = [ ]
+  for(var i = 0; i < cities.length; i++){
+  if (cities[i].temperature <= 70.0){
+    results.push(cities[i])
+    }
+  }
+  return results
+}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "cityNames" which takes an array of city objects like the
 // above problem and returns an array of the cities names.
+function cityNames() {
+  var cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 }
+  ]
+
+  let results = [ ]
+  for(var i = 0; i < cities.length; i++){
+    results.push(cities[i].name)
+  }
+  return results
+}
